a827d1cb23c5ced5f2f00e365578a78d278aa6e8
Jhon Gesell Villanueva Portella <unmsm182@gmail.com>
2020-09-28 12:21:06 -0500

    Widget de calibración de caudales
    Widget finalizado a nivel prototipo, toca compilar a continuación

--- projects_final/call_calibracion_n01.py
+++ projects_final/call_calibracion_n01.py
@@ -14,14 +14,16 @@ class MyForm(QDialog):
         variable1 = float(self.ui.lineEdit_flexim.text())
         variable2 = float(self.ui.lineEdit_sedapal.text())
         if variable1 < variable2:
-            variable3 = variable1/variable2
+            variable3 = (variable1/variable2)*100
+            variable4 = abs(100-variable3)
         elif variable2 < variable1:
-            variable3 = variable2/variable1
-        else:
-            variable3 = "son iguales ambos caudales"
-        self.ui.label_response1.setText(str(variable1))
-        self.ui.label_response2.setText(str(variable2))
-        self.ui.label_response3.setText(variable3)
+            variable3 = (variable2/variable1)*100
+            variable4 = abs(100-variable3)
+        #else:
+        #    variable3 = "son iguales ambos caudales"
+        self.ui.label_response1.setText(str(variable3))
+        self.ui.label_response2.setText(str(variable4))
+        self.ui.label_response3.setText(str(variable3 + variable4))
 if __name__=="__main__":
     app = QApplication(sys.argv)
     w = MyForm()
